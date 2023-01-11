c20410dee627c9fc965338cf92be71d36df34f26
root <root@40e7d8488113>
2023-01-11 02:24:22 +0000

    WheresWaldo(mouseX,mouseY), StringMethods(charAt, and length methods).

--- src/_02_boolean/_6_wheres_waldo/WheresWaldo.java
+++ src/_02_boolean/_6_wheres_waldo/WheresWaldo.java
@@ -5,6 +5,7 @@ import java.io.File;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
+import javax.swing.JOptionPane;
 
 import processing.core.PApplet;
 import processing.core.PImage;
@@ -38,78 +39,82 @@ import processing.core.PImage;
  *      playWhoohoo() method, otherwise call the playDoh() method.
  */
 public class WheresWaldo extends PApplet {
-    static final int WIDTH = 600;
-    static final int HEIGHT = 400;
+	static final int WIDTH = 600;
+	static final int HEIGHT = 400;
 
-    PImage waldo;
+	PImage waldo;
 
-    @Override
-    public void settings() {
-        size(WIDTH, HEIGHT);
-    }
+	@Override
+	public void settings() {
+		size(WIDTH, HEIGHT);
+	}
 
-    @Override
-    public void setup() {
-        waldo=loadImage("waldo.jpg");
-        size(600,400);
-        waldo.resize(600, 400);
-        background(waldo);
-    }
+	@Override
+	public void setup() {
+		waldo = loadImage("waldo.jpg");
+		size(600, 400);
+		waldo.resize(600, 400);
+		background(waldo);
+	}
 
-    @Override
-    public void draw() {
-        println("X: " + mouseX + "Y: " + mouseY);
-        if (mousePressed) {
-        	if ()
-        }
-    }
+	@Override
+	public void draw() {
+		println("X: " + mouseX + "Y: " + mouseY);
+		if (mousePressed) {
+			if (mouseX > 375 && mouseX < 420 && mouseY > 201 && mouseY < 279) {
 
-    static public void main(String[] args) {
-        PApplet.main(WheresWaldo.class.getName());
-    }
+				textSize(25);
+				text("You found Waldo!", 300, 200);
+			}
+		}
+	}
 
-    /*********************** DO NOT MODIFY THE CODE BELOW ********************/
+	static public void main(String[] args) {
+		PApplet.main(WheresWaldo.class.getName());
+	}
 
-    Clip clip = null;
+	/*********************** DO NOT MODIFY THE CODE BELOW ********************/
 
-    public void playDoh() {
-        if (clip == null || !clip.isActive()) {
-            clip = playSound("homer-doh.wav");
-        }
-    }
+	Clip clip = null;
 
-    public void playWhoohoo() {
-        if (clip == null || !clip.isActive()) {
-            clip = playSound("homer-woohoo.wav");
-        }
-    }
+	public void playDoh() {
+		if (clip == null || !clip.isActive()) {
+			clip = playSound("homer-doh.wav");
+		}
+	}
 
-    public Clip getSong(String fileName) {
-        String path = "src/";
-        Clip clip = null;
+	public void playWhoohoo() {
+		if (clip == null || !clip.isActive()) {
+			clip = playSound("homer-woohoo.wav");
+		}
+	}
 
-        // Note: use .wav files
-        try {
-            clip = AudioSystem.getClip();
-            AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path + fileName));
-            clip.open(inputStream);
-        } catch (Exception e) {
-            System.out.println("ERROR: " + e.getMessage() + " for " + path + fileName);
-        }
+	public Clip getSong(String fileName) {
+		String path = "src/";
+		Clip clip = null;
 
-        return clip;
-    }
+		// Note: use .wav files
+		try {
+			clip = AudioSystem.getClip();
+			AudioInputStream inputStream = AudioSystem.getAudioInputStream(new File(path + fileName));
+			clip.open(inputStream);
+		} catch (Exception e) {
+			System.out.println("ERROR: " + e.getMessage() + " for " + path + fileName);
+		}
 
-    public Clip playSound(String fileName) {
-        final Clip clip = getSong(fileName);
+		return clip;
+	}
 
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                clip.start();
-            }
-        }).start();
+	public Clip playSound(String fileName) {
+		final Clip clip = getSong(fileName);
 
-        return clip;
-    }
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				clip.start();
+			}
+		}).start();
+
+		return clip;
+	}
 }
